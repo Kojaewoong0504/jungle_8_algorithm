04641c6850256a614d92803d5017d92516c2a1de
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-03-26 09:31:20 +0900

    [Silver III] Title: 게임, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,20 @@
+x, y = map(int, input().split())
+
+start_rate = (y * 100) // x
+
+if x == y:
+    print(-1)
+elif start_rate >= 99:
+    if ((y + 10**9) * 100) // (x + 10**9) <= start_rate:
+        print(-1)
+else:
+    # 이분 탐색으로 최소 게임 수 찾기
+    start, end = 1, 10 ** 9
+    while start <= end:
+        mid = (start + end) // 2
+        new_z = ((y + mid) * 100) // (x + mid)
+        if new_z > start_rate:
+            end = mid - 1
+        else:
+            start = mid + 1
+    print(start)
