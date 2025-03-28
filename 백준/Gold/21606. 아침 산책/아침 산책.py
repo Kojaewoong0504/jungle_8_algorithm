d9af1c10e5106525973c61a52a51c907b2e0600b
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-03-28 09:36:57 +0900

    [Gold III] Title: 아침 산책, Time: 848 ms, Memory: 105764 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,38 +1,33 @@
 import sys
 
-input = sys.stdin.readline
-
-n = int(input())
-a = '0' + input()
-
-graph = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
-
-total = 0
-
-for _ in range(n - 1):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-
-    if a[x] == "1" and a[y] == "1":
-        total += 2
+sys.setrecursionlimit(10 ** 6)
+N = int(sys.stdin.readline())  # 정점수
+A = [0] + list(map(int, sys.stdin.readline().strip()))  # 실외 실내 정보를 노드 인덱스 1과 맞추기 위해 [0] 추가
+tree = [[] for _ in range(N + 1)]  # 빈공간 생성
+visit = [0] * (N + 1)  # 방문 체크
+ans = 0
+res = 0
+for _ in range(N - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    tree[a].append(b)
+    tree[b].append(a)
+    if A[a] == 1 and A[b] == 1:  # 둘다 실내일때 경우
+        ans += 2  # 서로 방문하는 케이스가 2개
 
 
-def dfs(node):
-    visited[node] = True
-    cnt = 0
-    for i in graph[node]:
-        if a[i] == "1":
+def dfs(v, cnt):  # cnt 실외와 연결된 실내 노드 개수
+    visit[v] = 1
+    for i in tree[v]:
+        if A[i] == 0 and visit[i] == 0:  # 내 외 내 내 외 일경우 마지막 외가 카운팅 안되는 반례
+            cnt = dfs(i, cnt)  # 해당 실외 기점으로 재귀
+        elif A[i] == 1 and visit[i] == 0:  # 실내면서 방문하지않았다면
             cnt += 1
-        elif not visited[i] and a[i] == "0":
-            cnt += dfs(i)
     return cnt
 
 
-for i in range(1, n+1):
-    if a[i] == "0" and not visited[i]:
-        result = dfs(i)
-        total += (result) * (result - 1)
+for i in range(1, N + 1):
+    if visit[i] == 0 and A[i] == 0:  # 방문안한 실외를 기준으로
+        x = dfs(i, 0)  # dfs시작 cnt는 시작이 0
+        res += x * (x - 1)  # 실외를 기준으로 인접한 실내 점 n개가 있을때 경로는 n(n-1)개이다
 
-print(total)
+print(res + ans)
