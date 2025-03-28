614e0a40076c1688df5b5509001f93109dbe64b7
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-03-28 09:34:35 +0900

    [Gold III] Title: 아침 산책, Time: 392 ms, Memory: 140240 KB, Score: 3 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -3,7 +3,7 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-a = '0' + input()
+a = [0] + list(map(int, input().strip()))
 
 graph = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
@@ -15,24 +15,23 @@ for _ in range(n - 1):
     graph[x].append(y)
     graph[y].append(x)
 
-    if a[x] == "1" and a[y] == "1":
+    if a[x] == 1 and a[y] == 1:
         total += 2
 
 
-def dfs(node):
+def dfs(node, cnt):
     visited[node] = True
-    cnt = 0
     for i in graph[node]:
-        if a[i] == "1":
+        if a[i] == 1 and not visited[i]:
             cnt += 1
-        elif not visited[i] and a[i] == "0":
-            cnt += dfs(i)
+        elif not visited[i] and a[i] == 0:
+            cnt += dfs(i, cnt)
     return cnt
 
 
 for i in range(1, n+1):
-    if a[i] == "0" and not visited[i]:
-        result = dfs(i)
+    if a[i] == 0 and not visited[i]:
+        result = dfs(i, 0)
         total += (result) * (result - 1)
 
 print(total)
