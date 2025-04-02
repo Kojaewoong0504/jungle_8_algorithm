ba8ea5a5de043f39b0c521537ec5f94ebd4f05bd
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-04-03 08:50:53 +0900

    [Gold IV] Title: 행성 연결, Time: 2316 ms, Memory: 290212 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.py
@@ -0,0 +1,37 @@
+n = int(input())
+
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+parent = [i for i in range(n+1)]
+
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x,y):
+    x, y = find(x), find(y)
+    if x < y:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+edges = []
+
+for i in range(n):
+    for j in range(n):
+        if not i == j:
+            edges.append((i,j,graph[i][j]))
+
+edges.sort(key=lambda x:x[2])
+
+cost = 0
+for edge in edges:
+    u, v, c = edge
+    if find(u) != find(v):
+        union(u,v)
+        cost += c
+
+print(cost)
