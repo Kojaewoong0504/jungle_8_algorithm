6c4d8e65f357a216763c79c09ba5ac855fa4ecc7
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-04-13 12:03:57 +0900

    [Silver I] Title: 지름길, Time: 100 ms, Memory: 110732 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,37 +1,37 @@
 import heapq
 
-n, d = map(int, input().split())
 
-graph = [[] for _ in range(d+1)]
-inf = float('INF')
+n, m = map(int, input().split())
 
+graph = [[] for _ in range(m+1)]
+INF = 10**8
 
 def dijkstra(start):
+    distance = [INF] * (m+1)
     q = []
-    heapq.heappush(q, (0,start))
+    heapq.heappush(q, (0, start))
     distance[start] = 0
+
     while q:
         dist, now = heapq.heappop(q)
-
         if dist > distance[now]:
             continue
 
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+        for next_node, weight in graph[now]:
+            cost = dist + weight
+            if cost < distance[next_node]:
+                distance[next_node] = cost
+                heapq.heappush(q, (cost, next_node))
+    return distance
 
-for i in range(d):
-    graph[i].append((i+1, 1))
+for i in range(m):
+    graph[i].append((i+1,1))
 
 for _ in range(n):
-    s, e, l = map(int, input().split())
-    if e > d:
+    a, b, c = map(int, input().split())
+    if b > m:
         continue
-    graph[s].append((e,l))
+    graph[a].append((b, c))
 
-
-distance = [inf] * (d+1)
-dijkstra(0)
-print(distance[d])
+result = dijkstra(0)
+print(result[m])
