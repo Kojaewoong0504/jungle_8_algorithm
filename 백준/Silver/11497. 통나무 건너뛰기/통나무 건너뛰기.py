0f3fa2ca5d62d4a76b11370892d40f513d2a2a3b
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-04-13 11:20:53 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 260 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -1,32 +1,29 @@
 from collections import deque
-
-
 t = int(input())
+
+def make(list, start):
+    is_left = start
+    result = deque()
+    for i in list:
+        if is_left:
+            result.append(i)
+            is_left = False
+        else:
+            result.appendleft(i)
+            is_left = True
+    max_data = 0
+    for i in range(1, len(list)):
+        max_data = max(max_data, abs(result[i-1] - result[i]))
+    max_data = max(max_data, abs(result[0] - result[-1]))
+    return max_data
+
 
 for _ in range(t):
     n = int(input())
+    logs = list(map(int, input().split()))
+    logs.sort(reverse=True)
 
-    log_list = sorted(list(map(int, input().split())), reverse=True)
+    start_left = make(logs, True)
+    start_right = make(logs, False)
 
-    result = []
-    for i in range(2):
-        q = deque()
-        left = i
-        for log in log_list:
-            if left == 1:
-                q.appendleft(log)
-                left = 0
-            else:
-                q.append(log)
-                left = 1
-        result.append(q)
-
-    min_result = float('INF')
-
-    for res in result:
-        max_res = 0
-        for i in range(1, len(res)):
-            max_res = max(max_res, abs(res[i-1] - res[i]))
-        max_res = max(max_res, abs(res[0] - res[-1]))
-        min_result = min(max_res, min_result)
-    print(min_result)
+    print(min(start_left, start_right))
