59c1a7fc8026ca4d4014acec126c23b4bb3eb3d5
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-05-29 08:55:20 +0900

    [Bronze I] Title: 유진수, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1356. 유진수/유진수.py
@@ -0,0 +1,13 @@
+N = input()
+is_true = 0
+for i in range(1, len(N)):
+    s1, s2 = N[:i], N[i:]
+    m1 = m2 = 1
+    for n in s1:
+        m1 *= int(n)
+    for n in s2:
+        m2 *= int(n)
+    if m1 == m2:
+        is_true = 1
+        break
+print("YES" if is_true else "NO")
