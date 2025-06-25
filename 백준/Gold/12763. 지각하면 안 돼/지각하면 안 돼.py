373703e5c94d78bd0e87980db820b2d6220ae499
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-06-26 08:53:08 +0900

    [Gold II] Title: 지각하면 안 돼, Time: 728 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/12763. 지각하면 안 돼/지각하면 안 돼.py
@@ -0,0 +1,41 @@
+import sys
+
+sys.setrecursionlimit(10 ** 9)
+input = sys.stdin.readline
+
+
+def dfs(node, T, C):
+    global answer
+
+    if node == n - 1:
+        answer = min(answer, C)
+        return
+    for nxt, time, cost in graph[node]:
+        if not visited[nxt]:
+            if T + time <= t and C + cost <= m:
+                visited[nxt] = True
+                dfs(nxt, T + time, C + cost)
+                visited[nxt] = False
+
+
+n = int(input())
+t, m = map(int, input().split())
+l = int(input())
+INF = int(1e9)
+
+graph = [[] for _ in range(n)]
+for _ in range(l):
+    a, b, time, cost = map(int, input().split())
+    a -= 1
+    b -= 1
+    graph[a].append((b, time, cost))
+    graph[b].append((a, time, cost))
+
+answer = INF
+visited = [False] * n
+visited[0] = True
+dfs(0, 0, 0)
+if answer == INF:
+    print(-1)
+else:
+    print(answer)
