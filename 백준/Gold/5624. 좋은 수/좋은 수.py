9edccb20fab870bc31a0b7e6be95f043273a5a7f
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-08-15 14:22:18 +0900

    [Gold III] Title: 좋은 수, Time: 164 ms, Memory: 122960 KB -BaekjoonHub

--- 백준/Gold/5624. 좋은 수/좋은 수.py
+++ 백준/Gold/5624. 좋은 수/좋은 수.py
@@ -1,17 +1,26 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = list(map(int,input().split()))
- 
-one,two = set(),set()
-dp = [False]*n
- 
-for i in range(n):
-    a = arr[i]
-    for o in one:
-        if a-o in two:
-            dp[i] = True
- 
+arr = list(map(int, input().split()))
+
+one, two = set(), set()
+ans = 0
+
+for a in arr:
+    is_good = False
+    # 판정: A[i] = s + (p+q)  <=>  A[i]-s in two
+    for s in one:
+        if (a - s) in two:
+            is_good = True
+            break
+    if is_good:
+        ans += 1
+
+    # 갱신 (현재 a는 다음 원소 판정부터 사용)
+    # 먼저 one에 넣고, one의 모든 s에 대해 a+s를 two에 추가
     one.add(a)
-    for o in one:
-        two.add(a+o)
- 
-print(sum(dp))
+    for s in one:
+        two.add(a + s)
+
+print(ans)
