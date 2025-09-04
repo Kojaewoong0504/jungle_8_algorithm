faf72c9566008d19091dd6df06e32a1b4d671420
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-09-04 11:19:36 +0900

    [Silver IV] Title: 선택의 기로, Time: 3252 ms, Memory: 92292 KB -BaekjoonHub

---
+++ 백준/Silver/30970. 선택의 기로/선택의 기로.py
@@ -0,0 +1,14 @@
+import copy
+
+n = int(input())
+items = [list(map(int, input().split())) for _ in range(n)]
+
+
+first = copy.deepcopy(items)
+second = copy.deepcopy(items)
+
+first.sort(key=lambda x: (-x[0], x[1]))
+second.sort(key=lambda x: (x[1], -x[0]))
+
+print(f"{first[0][0]} {first[0][1]} {first[1][0]} {first[1][1]}")
+print(f"{second[0][0]} {second[0][1]} {second[1][0]} {second[1][1]}")
