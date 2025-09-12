325ee5c97be36564205e3d2e9b0651fee230c7f2
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-09-12 11:56:20 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    if n < 3:
+        print(1)
+        continue
+    dp = [0] * n
+    for i in range(3):
+        dp[i] = 1
+
+    for i in range(3, n):
+        dp[i] = dp[i-3] + dp[i-2]
+
+    print(dp[n-1])
