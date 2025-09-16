c04f68a02172c0699496651cc865699a223d508a
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-09-16 11:17:12 +0900

    [Silver II] Title: 알파벳 블록, Time: 432 ms, Memory: 222148 KB -BaekjoonHub

--- 백준/Silver/27497. 알파벳 블록/알파벳 블록.py
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.py
@@ -8,22 +8,23 @@ q = deque()
 ins = []
 
 for _ in range(n):
-    parts = input().split()  # 공백/개행 모두 안전하게 분리
-    if parts[0] == "1":
-        q.append(parts[1])   # parts[1]는 이미 'a' 형태
-        ins.append(1)        # 뒤에 넣었음을 기록
-    elif parts[0] == "2":
-        q.appendleft(parts[1])
-        ins.append(0)        # 앞에 넣었음을 기록
-    else:  # "3"
+    parts = input().split()
+    if parts[0] in ["1", "2"]:
+        if parts[0] == "1":
+            q.append(parts[1])
+            ins.append(1)
+        else:
+            q.appendleft(parts[1])
+            ins.append(0)
+    else:
         if ins:
-            last = ins.pop()     # 항상 끝에서 제거 (스택)
+            last = ins.pop()
             if last == 1:
                 q.pop()
             else:
                 q.popleft()
 
-if q:
+if len(q) > 0:
     print("".join(q))
 else:
     print(0)
