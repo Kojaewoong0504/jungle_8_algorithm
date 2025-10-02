686c642190895e728de01a09642e532631746ad2
Gojaewoong <jaewoong.ko0504@gmail.com>
2025-10-02 11:01:45 +0900

    [Silver III] Title: 다이나믹이 뭐예요?, Time: 108 ms, Memory: 116544 KB -BaekjoonHub

---
+++ 백준/Silver/14494. 다이나믹이 뭐예요？/다이나믹이 뭐예요？.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+MOD = 1_000_000_007
+
+dp = [[0]*(m+1) for _ in range(n+1)]
+dp[1][1] = 1
+
+for i in range(2, n+1):
+    dp[i][1] = 1
+for j in range(2, m+1):
+    dp[1][j] = 1
+
+for i in range(2, n+1):
+    for j in range(2, m+1):
+        dp[i][j] = (dp[i-1][j] + dp[i][j-1] + dp[i-1][j-1]) % MOD
+
+print(dp[n][m] % MOD)
