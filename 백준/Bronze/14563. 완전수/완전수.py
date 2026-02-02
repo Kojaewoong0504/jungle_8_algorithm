d88e3fe0b47e942a7a79db9904665c7e2bf1e778
Gojaewoong <jaewoong.ko0504@gmail.com>
2026-02-02 10:16:22 +0900

    [Bronze II] Title: 완전수, Time: 180 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14563. 완전수/완전수.py
@@ -0,0 +1,13 @@
+T = int(input())
+N = list(map(int, input().split()))
+for n in N:
+    sum = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum += i
+    if sum == n:
+        print('Perfect')
+    elif sum > n:
+        print('Abundant')
+    else:
+        print('Deficient')
