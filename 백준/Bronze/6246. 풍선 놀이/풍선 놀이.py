069f34da7da4b69001d67f3bc739add1481d611e
Gojaewoong <jaewoong.ko0504@gmail.com>
2026-02-14 10:34:37 +0900

    [Bronze II] Title: 풍선 놀이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6246. 풍선 놀이/풍선 놀이.py
@@ -0,0 +1,8 @@
+n, q = map(int, input().split())
+balloons = [1]*(n+1)
+balloons[0] = 0
+for _ in range(q):
+    l, i = map(int, input().split())
+    for j in range(l, n+1, i):
+        balloons[j] = 0
+print(sum(balloons))
