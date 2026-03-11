84a545a84e1318e6c18801325d935c7e640733f0
Gojaewoong <jaewoong.ko0504@gmail.com>
2026-03-11 12:21:36 +0900

    [Silver III] Title: 합 구하기, Time: 156 ms, Memory: 124404 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -1,10 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 arr = list(map(int, input().split()))
 m = int(input())
-prefix = [0 for _ in range(n + 1)]
+
+prefix = [0] * (n + 1)
+
 for i in range(1, n + 1):
     prefix[i] = prefix[i - 1] + arr[i - 1]
 
+result = []
+
 for _ in range(m):
     i, j = map(int, input().split())
-    print(prefix[j]- prefix[i-1])
+    result.append(str(prefix[j] - prefix[i - 1]))
+
+print("\n".join(result))
